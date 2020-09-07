f7e16550153e1c888367ba955ef38a57838c9fc4
Burton Johnsey <git@tonjohn.com>
2020-09-06 22:32:42 -0700

    h1 styling and featured cleanup

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h1 class="title">
+    <h1 class="text-center text-3xl title">
       <span class="text-purple-600">arisa</span><span>blogs</span>
     </h1>
+    <!-- FEATURED -->
+
     <div
       class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
     >
@@ -28,56 +30,6 @@
         >
           <article-card :article="articles[0]"></article-card>
           <article-card :article="articles[1]"></article-card>
-          <!-- <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-            <div class="flex-shrink-0">
-              <img
-                class="h-48 w-full object-cover"
-                src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-                alt=""
-              />
-            </div>
-            <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-              <div class="flex-1">
-                <p class="text-sm leading-5 font-medium text-indigo-600">
-                  <a href="#" class="hover:underline"> Video </a>
-                </p>
-                <a href="#" class="block">
-                  <h3
-                    class="mt-2 text-xl leading-7 font-semibold text-gray-900"
-                  >
-                    How to use search engine optimization to drive sales
-                  </h3>
-                  <p class="mt-3 text-base leading-6 text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Velit facilis asperiores porro quaerat doloribus, eveniet
-                    dolore. Adipisci tempora aut inventore optio animi., tempore
-                    temporibus quo laudantium.
-                  </p>
-                </a>
-              </div>
-              <div class="mt-6 flex items-center">
-                <div class="flex-shrink-0">
-                  <a href="#">
-                    <img
-                      class="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </a>
-                </div>
-                <div class="ml-3">
-                  <p class="text-sm leading-5 font-medium text-gray-900">
-                    <a href="#" class="hover:underline"> Brenna Goyette </a>
-                  </p>
-                  <div class="flex text-sm leading-5 text-gray-500">
-                    <time datetime="2020-03-16"> Mar 16, 2020 </time>
-                    <span class="mx-1"> &middot; </span>
-                    <span> 6 min read </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> -->
           <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
             <div class="flex-shrink-0">
               <img
