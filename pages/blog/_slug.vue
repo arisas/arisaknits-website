912efc1acf3855e7f06d87e3b2a02cae870905b0
Burton Johnsey <git@tonjohn.com>
2020-09-12 12:56:46 -0700

    metadata

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,10 +1,15 @@
 <template>
-  <article>
-    <nuxt-content
-      class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto"
-      :document="article"
-    />
-  </article>
+  <div>
+    <div class="title">
+      <span class="text-purple-600">arisa</span><span>blogs</span>
+    </div>
+    <article>
+      <nuxt-content
+        class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto"
+        :document="article"
+      />
+    </article>
+  </div>
 </template>
 
 <script>
@@ -13,6 +18,36 @@ export default {
     const article = await $content('articles', params.slug).fetch()
 
     return { article }
+  },
+  head() {
+    return {
+      title: this.article.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.article.description,
+        },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.article.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.article.description,
+        },
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.article.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.article.description,
+        },
+      ],
+    }
   },
 }
 </script>
