4602f0fa74ea9c976cfc457f796aa8bd37aaaa81
arisas <arisa@arisaknits.com>
2023-06-28 20:05:32 -0700

    fixed screenshots on row counter page

--- pages/rowcounter/index.vue
+++ pages/rowcounter/index.vue
@@ -52,17 +52,17 @@ export default {
         {
           alt: 'ArisaKnits Row Counter App Screenshot with Zero',
           title: 'ArisaKnits Row Counter App Screenshot with Zero',
-          src: 'static\img\v1.2screenshot_zero.png',
+          src: '~static\img\v1.2screenshot_zero.png',
         },
         {
           alt: 'ArisaKnits Row Counter App Screenshot with Increase Numbers',
           title: 'ArisaKnits Row Counter App Screenshot with Increase Numbers',
-          src: 'static\img\v1.2screenshot_increase.png',
+          src: '~static\img\v1.2screenshot_increase.png',
         },
         {
           alt: 'ArisaKnits Row Counter App Screenshot with Reset Row Counter Button',
           title: 'ArisaKnits Row Counter App Screenshot with Reset Row Counter Button',
-          src: 'static\img\v1.2screenshot_resetbutton.png',
+          src: '~static\img\v1.2screenshot_resetbutton.png',
         },
       ],
     }
