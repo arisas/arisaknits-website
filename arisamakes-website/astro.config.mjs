417c627636848abee2fb918bb0d622973a60e52b
arisas <arisa@arisaknits.com>
2026-07-14 10:18:33 -0700

    Migrating website from Nuxt to Astro (#26)
    * upgrade tailwindcss to 4.0
    
    * created new astro project and work on front page
    
    * added new font and more work on front page
    
    css, new font, more text to front page
    
    * finished v1 of front page and privacy policy
    
    * download button on pattern page now works
    
    * new font and updated row counter page
    
    * finish updating row counter page and tweaks to front page and layouts
    
    * adjust layout on bipocmonthly, frontp age, watchos, and pattern
    
    * update colors and front page text on the 3.0 update
    
    * added call out for testing 3.0!
    
    * Update design on multiple pages, added new blog page, and  change url to bipoccraftcircle
    
    * added dynamic meta titles for each page!
    
    * edit spacing in about me links
    
    * changes for deployment to cloudflare
    
    * support for smaller images on phone screen and link to classic site

---
+++ arisamakes-website/astro.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Lato",
+    cssVariable: "--font-lato",
+      },
+
+      {
+    provider: fontProviders.fontsource(),
+    name: "Itim",
+    cssVariable: "--font-itim",
+      },
+
+      {
+    provider: fontProviders.fontsource(),
+    name: "Playpen Sans Thai",
+    cssVariable: "--font-playpen-sans-thai",
+      },
+
+      {
+    provider: fontProviders.fontsource(),
+    name: "Caveat",
+    cssVariable: "--font-caveat",
+      },
+
+      {
+    provider: fontProviders.fontsource(),
+    name: "LXGW Marker Gothic",
+    cssVariable: "--font-lxgw-marker-gothic",
+      }
+    ],
+
+  adapter: cloudflare(),
+});
