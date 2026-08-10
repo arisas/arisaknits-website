33634d847d07b20a5153696c908d2a6fe567486f
Burton <git@tonjohn.com>
2026-08-10 02:21:29 -0700

    configure image service

--- arisamakes-website/astro.config.mjs
+++ arisamakes-website/astro.config.mjs
@@ -2,44 +2,51 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  fonts: [{
-    provider: fontProviders.fontsource(),
-    name: "Lato",
-    cssVariable: "--font-lato",
-      },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Lato',
+      cssVariable: '--font-lato',
+    },
 
-      {
-    provider: fontProviders.fontsource(),
-    name: "Itim",
-    cssVariable: "--font-itim",
-      },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Itim',
+      cssVariable: '--font-itim',
+    },
 
-      {
-    provider: fontProviders.fontsource(),
-    name: "Playpen Sans Thai",
-    cssVariable: "--font-playpen-sans-thai",
-      },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Playpen Sans Thai',
+      cssVariable: '--font-playpen-sans-thai',
+    },
 
-      {
-    provider: fontProviders.fontsource(),
-    name: "Caveat",
-    cssVariable: "--font-caveat",
-      },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Caveat',
+      cssVariable: '--font-caveat',
+    },
 
-      {
-    provider: fontProviders.fontsource(),
-    name: "LXGW Marker Gothic",
-    cssVariable: "--font-lxgw-marker-gothic",
-      }
-    ],
+    {
+      provider: fontProviders.fontsource(),
+      name: 'LXGW Marker Gothic',
+      cssVariable: '--font-lxgw-marker-gothic',
+    },
+  ],
 
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
+  image: {
+    // responsiveStyles: true,
+    layout: 'constrained',
+  },
 });
